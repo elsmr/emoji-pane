a2c3e9134fe3a62a70d51dcb5340c291cee34c0b
Elias Meire <me@eliasmei.re>
2017-01-20 01:17:11 +0100

    queue method for recents

--- src/utils/emoji.js
+++ src/utils/emoji.js
@@ -4,3 +4,20 @@ export const matches = (emoji, filter) => {
   emoji.desc.indexOf(lFilter) !== -1 ||
   emoji.keywords.some(el => el.indexOf(lFilter) !== -1);
 };
+
+export const queue = (arr, maxLen, emoji) => {
+  arr.some((el, i) => {
+    const contains = el.char === emoji.char;
+    if (contains) {
+      arr.splice(i, 1);
+    }
+    return contains;
+  });
+
+  if (arr.length >= maxLen) {
+    arr.shift();
+  }
+
+  arr.push(emoji);
+  return arr;
+};
