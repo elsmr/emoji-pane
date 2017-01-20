0bd40943a39bb977e3a290fb7102567915d1aff9
Elias Meire <me@eliasmei.re>
2017-01-20 01:17:47 +0100

    more stories

--- stories/index.js
+++ stories/index.js
@@ -2,7 +2,43 @@ import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import EmojiPane from '../src/components/EmojiPane';
 
+const recents = [{
+      "name": "yum",
+      "char": "😋",
+      "code": "1f60b",
+      "desc": "face savouring delicious food",
+      "i": "11",
+      "keywords": [
+        "happy",
+        "silly",
+        "smiley",
+        "emotion",
+        "sarcastic",
+        "good"
+      ]
+    },
+    {
+      "name": "sunglasses",
+      "char": "😎",
+      "code": "1f60e",
+      "desc": "smiling face with sunglasses",
+      "i": "12",
+      "keywords": [
+        "silly",
+        "smiley",
+        "emojione",
+        "glasses",
+        "boys night"
+      ]
+    }];
+
 storiesOf('EmojiPane', module)
   .add('default', () => (
     <EmojiPane onSelected={action('selected')} />
+  ))
+  .add('with custom skin tone', () => (
+    <EmojiPane onSelected={action('selected')} skinTone={3} />
+  ))
+  .add('with recents', () => (
+    <EmojiPane onSelected={action('selected')} recents={recents} />
   ));
