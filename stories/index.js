56314d480ca67892b1f86285e8283aa453777f80
Elias Meire <me@eliasmei.re>
2017-01-27 16:15:07 +0100

    indent

--- stories/index.js
+++ stories/index.js
@@ -3,34 +3,34 @@ import { storiesOf, action } from '@kadira/storybook';
 import EmojiPane from '../src/components/EmojiPane';
 
 const recents = [{
-      "name": "yum",
-      "char": "😋",
-      "code": "1f60b",
-      "desc": "face savouring delicious food",
-      "i": "11",
-      "keywords": [
-        "happy",
-        "silly",
-        "smiley",
-        "emotion",
-        "sarcastic",
-        "good"
-      ]
-    },
-    {
-      "name": "sunglasses",
-      "char": "😎",
-      "code": "1f60e",
-      "desc": "smiling face with sunglasses",
-      "i": "12",
-      "keywords": [
-        "silly",
-        "smiley",
-        "emojione",
-        "glasses",
-        "boys night"
-      ]
-    }];
+  name: 'yum',
+  char: '😋',
+  code: '1f60b',
+  desc: 'face savouring delicious food',
+  i: '11',
+  keywords: [
+    'happy',
+    'silly',
+    'smiley',
+    'emotion',
+    'sarcastic',
+    'good',
+  ],
+},
+{
+  name: 'sunglasses',
+  char: '😎',
+  code: '1f60e',
+  desc: 'smiling face with sunglasses',
+  i: '12',
+  keywords: [
+    'silly',
+    'smiley',
+    'emojione',
+    'glasses',
+    'boys night',
+  ],
+}];
 
 storiesOf('EmojiPane', module)
   .add('default', () => (
