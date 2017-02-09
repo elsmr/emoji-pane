93d9e6afb74b761921cded22ac593b035a727d80
Elias Meire <me@eliasmei.re>
2017-02-09 16:01:34 +0100

    new index file with exports

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import EmojiPane from './components/EmojiPane';
+import Emoji from './components/Emoji';
+import EmojiList from './components/EmojiList';
+import EmojiCategory from './components/EmojiCategory';
+import CategoryList from './components/CategoryList';
+import CategorySwitcher from './components/CategorySwitcher';
+import SearchBar from './components/SearchBar';
+import SearchResults from './components/SearchResults';
+
+export {
+  EmojiPane,
+  Emoji,
+  EmojiList,
+  EmojiCategory,
+  CategoryList,
+  CategorySwitcher,
+  SearchBar,
+  SearchResults,
+};
+
+export default EmojiPane;
